ff0cf8369d45fcaa7f25de4c5c33e37d9c494afb
Jackwannsee <46992531+Jackwannsee@users.noreply.github.com>
2020-02-28 18:44:23 +0530

    Update Ships.java

--- Ships.java
+++ Ships.java
@@ -1,41 +1,53 @@
+import java.util.Scanner;
+import java.math.RoundingMode;
+import java.text.DecimalFormat;
+
 public class Ships
 {
+    Scanner in = new Scanner(System.in);
+    private static DecimalFormat df2 = new DecimalFormat("#.##");
+
     private String Name;
     private double HP;
     private double ArmorPoints;
+    private double Damage;
     private int Cost;
-
-    Ships()
-    {
-        String[] _name = {"Star Fighter", "The Chosen One", "Master Palpatine", "General Taco", "Master Yoda"};
-        int Number = (int)(Math.random()*4) + 1;
-        Name = _name[Number];
-
-        HP = (int)(Math.random()*2000)+ 1300;
-        ArmorPoints = (int)(Math.random()*750) + 250;
-    }
+    private Ships[] ShipShop = new Ships[100];
 
     Ships(String _Name)
     {
         Name = _Name;
         HP = (int)(Math.random()*2000)+ 1300;
         ArmorPoints = (int)(Math.random()*750) + 250;
+        Damage = 250;
     }
 
-    Ships(String _Name, double _HP, double _ArmorPoints, int _Cost)
+    Ships(String _Name, double _HP, double _ArmorPoints, int _Cost, double _Damage)
     {
         Name = _Name;
         HP = _HP;
         ArmorPoints = _ArmorPoints;
         Cost = _Cost;
+        Damage = _Damage;
     }
 
-    void Information()
+    Ships(double _HP, double _ArmorPoints, double _Damage)
     {
-        System.out.println(" *  " + Name + "  *  HP: " + HP + "  *  Armor: " + ArmorPoints);
+        String[] _name = {"Star Fighter", "The Chosen One", "Master Palpatine", "General Taco", "Master Yoda"};
+        int Number = (int)(Math.random()*4) + 1;
+        Name = _name[Number];
+
+        HP = _HP;
+        ArmorPoints = _ArmorPoints;
+        Damage = _Damage;
     }
 
-    void InformationCost() {System.out.println(" *  " + Name + "  *  HP: " + HP + "  *  Armor: " + ArmorPoints + "  *  Cost: " + Cost); }
+    void Information() {
+        System.out.println(" *  " + Name + "  *  HP: " + df2.format(HP) + "  *  Armor: " + df2.format(ArmorPoints)+ "  *  Attack: " + Damage);
+    }
+
+    void InformationCost() {
+        System.out.println(" *  " + Name + "  *  HP: " + HP + "  *  Armor: " + ArmorPoints + "  *  Attack: " + Damage + "  *  Cost: " + Cost); }
 
     public String getName() {
         return Name;
@@ -67,5 +79,14 @@ public class Ships
 
     public void setCost(int cost) {
         Cost = cost;
+    }
+
+    public double getDamage() {
+
+        return Damage;
+    }
+
+    public void setDamage(double damage) {
+        Damage = damage;
     }
 }
